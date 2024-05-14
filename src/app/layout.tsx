9736df89d326a62b79b9f90d94b78b5d878b451f
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-05-14 16:11:14 -0300

    gtm tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,39 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-
-        {/* GTM */}
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NPWZWDR8'); 
-              `,
-          }}
-        /> */}
       </head>
 
       <body className={mont.className}>
-        {/*  <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPWZWDR8" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        /> */}
-
-        {/* ---------------- */}
-
         {/* GTM */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-NPWZWDR8`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Script
           id="gtm-script"
           strategy="afterInteractive"
@@ -62,10 +33,18 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'GTM-NPWZWDR8');
+            })(window,document,'script','dataLayer', 'GTM-PZ3X3GNJ');
             `,
           }}
         />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-PZ3X3GNJ`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
         {/* GTAG */}
         <Script
