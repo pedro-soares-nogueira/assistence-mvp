b66bed46f200781f1743124adf8541b30f66e497
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-05-14 15:53:37 -0300

    testing gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 const mont = Montserrat({ subsets: ["latin"] });
 
@@ -21,24 +22,48 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
 
         {/* GTM */}
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NPWZWDR8');
+              })(window,document,'script','dataLayer','GTM-NPWZWDR8'); 
               `,
           }}
-        />
+        /> */}
       </head>
 
       <body className={mont.className}>
         {/* GTM */}
-        <noscript
+        {/*  <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPWZWDR8" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        /> */}
+
+        {/* ---------------- */}
+
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-NPWZWDR8`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'GTM-NPWZWDR8');
+            `,
           }}
         />
 
