4ca0505656981b7126fcca9bf00f31bd3a5d2e30
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-08-04 20:50:07 -0300

    new rpdiecare home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import * as Dialog from "@radix-ui/react-dialog";
+import React, { useEffect, useState } from "react";
 import { Image as PImage, CheckCircle } from "phosphor-react";
 import Link from "next/link";
-import main_logo from "@/assets/logo.svg";
-import Image from "next/image";
-import main_hero from "@/assets/main_hero.png";
-import main_hero_mob from "@/assets/main_hero_mob.svg";
 import { BounceLoader } from "react-spinners";
 import * as Select from "@radix-ui/react-select";
 
@@ -77,11 +72,12 @@ export interface IPartiner {
   };
 }
 
-export default function Home() {
+const TestingHome = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [partners, setPartners] = useState<IPartiner[]>([]);
   const [specialtyToFilter, setSpecialtyToFilter] = useState("");
   const [nameToFilter, setNameToFilter] = useState("");
+  const [searchTriggered, setSearchTriggered] = useState(false);
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6;
@@ -131,14 +127,11 @@ export default function Home() {
     });
   }
 
-  const filteredPartners = filterPartners(
-    partners,
-    specialtyToFilter,
-    nameToFilter
-  );
+  const filteredPartners = searchTriggered
+    ? filterPartners(partners, specialtyToFilter, nameToFilter)
+    : partners;
 
-  const partnersToRender =
-    filteredPartners.length === 0 ? partners : filteredPartners;
+  const partnersToRender = filteredPartners;
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -149,101 +142,45 @@ export default function Home() {
 
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
+    window.scrollTo({ top: 300, behavior: "smooth" });
+  };
 
-    window.scrollTo({ top: 1300, behavior: "smooth" });
+  const handleSearch = () => {
+    setSearchTriggered(true);
+    setCurrentPage(1);
   };
 
   const totalPages = Math.ceil(partnersToRender.length / itemsPerPage);
-  const maxPageButtons = 2;
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
 
   return (
-    <main className="">
-      <section className="text-gray-600 body-font">
-        {/* Desktop */}
-        <div
-          className="hidden lg:block"
-          style={{
-            backgroundImage: `url('${main_hero.src}')`,
-            backgroundRepeat: "no-repeat",
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#F1F1F1",
-          }}
-        >
-          <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 px-5 py-8 justify-center items-center relative h-[33.5rem]">
-            <div className="hidden lg:block" />
-            <div className="w-full flex flex-col items-start justify-start gap-8">
-              <a className="border border-[#e7e7e7] title-font font-medium items-center text-gray-900 hidden lg:flex">
-                <Image
-                  alt=""
-                  src={main_logo}
-                  className="max-w-[145px] w-full"
-                />
-              </a>
-              <h1 className="title-font sm:text-4xl text-3xl font-bold text-gray-900 text-center lg:text-left">
-                Portal de Saúde para a Comunidade LGBT+
-              </h1>
-              <p className="leading-relaxed text-sm  text-center lg:text-left">
-                Somos um portal que visa conectar a comunidade LGBT+ a uma lista
-                de profissionais de saúde comprometidos em fornecer atendimento
-                de baixo custo e seguro para todos. <br />
-                <br /> Atuamos como uma plataforma de conexão entre membros da
-                comunidade e profissionais de saúde que demonstram compreensão e
-                sensibilidade em relação às questões de diversidade sexual e de
-                gênero.
-              </p>
+    <>
+      <section
+        className="relative overflow-hidden bg-gradient-to-b from-[#222C60] to-[#1B1E27] 
+                          min-h-[28rem] flex items-center justify-center text-center"
+      >
+        <div className="md:max-w-[58rem] mx-auto px-4 space-y-12 w-full">
+          <div className="space-y-2 text-sm">
+            <div className="text-[#E6C7FF] uppercase text-base md:text-[20px]">
+              Saúde LGBTI+
             </div>
+            <h2 className="heading-bold font-semibold text-[34px] leading-[40px] sm:text-[38px] sm:leading-[46px] md:font-bold md:text-[48px] md:leading-[55px] mx-auto text-white">
+              Busque profissionais inclusivos
+            </h2>
           </div>
-        </div>
-
-        {/* Mobile */}
-        <div className="block lg:hidden bg-[#F1F1F1]">
-          <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 px-5 pt-8 justify-center items-center relative">
-            <div className="hidden lg:block" />
-            <div className="w-full flex flex-col items-start justify-start gap-8">
-              <h1 className="title-font sm:text-4xl text-3xl font-bold text-gray-900 text-center lg:text-left">
-                Portal de Saúde para a Comunidade LGBT+
-              </h1>
-              <p className="leading-relaxed text-sm  text-center lg:text-left">
-                Somos um portal que visa conectar a comunidade LGBT+ a uma lista
-                de profissionais de saúde comprometidos em fornecer atendimento
-                de baixo custo e seguro para todos. <br /> <br />
-                Atuamos como uma plataforma de conexão entre membros da
-                comunidade e profissionais de saúde que demonstram compreensão e
-                sensibilidade em relação às questões de diversidade sexual e de
-                gênero.
-              </p>
-              <div className="h-[405px] object-cover mx-auto">
-                <Image alt="" src={main_hero_mob} className="w-full mx-auto" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 mx-auto mt-20">
-          <div className="flex flex-col text-center w-full mb-14">
-            <h1 className="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900">
-              Time de parceiros
-            </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-sm">
-              Escolha o profissional de saúde que melhor atenda às suas
-              necessidades. Depois, entre em contato diretamente pelo WhatsApp
-              ou redes sociais listadas em seus perfis. <br />
-              Estamos aqui para facilitar sua busca por cuidados de saúde
-              acessíveis e seguros. Não hesite em dar o primeiro passo em
-              direção ao cuidado que você merece.
-            </p>
-          </div>
-
-          <div className="mb-8 grid grid-cols-1 md:grid-cols-3 gap-2">
+          <div className="mb-8 grid grid-cols-1 md:grid-cols-6 lg:grid-cols-7 gap-2">
             <input
               type="text"
               placeholder="Buscar por nome"
               value={nameToFilter}
               onChange={(e) => setNameToFilter(e.target.value)}
-              className="border border-gray-300 rounded-md py-3 px-4 text-sm w-full"
+              onKeyDown={handleKeyDown}
+              className="border border-gray-300 rounded-[2px] py-3 px-4 text-sm w-full md:col-span-3"
             />
 
             <Select.Root
@@ -251,14 +188,14 @@ export default function Home() {
               onValueChange={setSpecialtyToFilter}
             >
               <Select.Trigger
-                className="border border-gray-300 rounded-md py-3 px-4 flex items-center 
-                        justify-between gap-5 text-sm  w-full"
+                className="border border-gray-300 rounded-[2px] py-3 px-4 flex items-center 
+                        justify-between gap-5 text-sm w-full md:col-span-3 bg-white"
               >
                 <Select.Value placeholder="Selecione uma especialidade" />
                 <Select.Icon className="" />
               </Select.Trigger>
               <Select.Portal>
-                <Select.Content className="bg-white px-4 py-2 drop-shadow-lg rounded-md">
+                <Select.Content className="bg-white px-4 py-2 drop-shadow-lg rounded-[2px]">
                   <Select.ScrollUpButton />
                   <Select.Viewport>
                     {specialtiesToFilter.length !== 0 &&
@@ -285,22 +222,45 @@ export default function Home() {
               </Select.Portal>
             </Select.Root>
 
-            <button
-              onClick={() => {
-                setSpecialtyToFilter("");
-                setNameToFilter("");
-              }}
-              className="float-end sm:float-none py-3 px-4 rounded-md border-gray-300"
-            >
-              Limpar
-            </button>
-          </div>
+            <div className="flex gap-2 flex-col sm:flex-row md:col-span-6 lg:col-span-1">
+              <button
+                onClick={handleSearch}
+                className="w-full py-[8px] px-4 rounded-[2px] border border-white text-white "
+              >
+                Buscar
+              </button>
 
+              <button
+                onClick={() => {
+                  setSpecialtyToFilter("");
+                  setNameToFilter("");
+                  setSearchTriggered(false);
+                }}
+                className="w-full py-[8px] px-4 rounded-[2px] text-white"
+              >
+                Limpar
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute bottom-0 inset-x-0 w-full flex items-center justify-between">
+          <div className="bg-[#7E34D9] max-w-[900px] w-full min-h-[8px]" />
+          <div className="bg-[#0074A6] max-w-[900px] w-full min-h-[8px]" />
+          <div className="bg-[#79A06E] max-w-[900px] w-full min-h-[8px]" />
+          <div className="bg-[#FFB101] max-w-[900px] w-full min-h-[8px]" />
+          <div className="bg-[#FF6001] max-w-[900px] w-full min-h-[8px]" />
+          <div className="bg-[#CF1616] max-w-[900px] w-full min-h-[8px]" />
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="sm:container px-5 mx-auto mt-[2rem] md:mt-20">
           <BounceLoader
             size={150}
             loading={isLoading}
             className="mx-auto mb-14"
-            color="#e9d5ff"
+            color="#7E34D9"
           />
 
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
@@ -313,46 +273,57 @@ export default function Home() {
               return (
                 <div
                   key={index}
-                  className="space-y-4 border border-[#F5F5F5] p-5 rounded-[2px]"
+                  className="border border-[#F5F5F5] p-5 rounded-[2px] flex flex-col justify-between gap-4"
                 >
-                  <div className="flex">
-                    {url !== null ? (
-                      <img
-                        alt="team"
-                        className="w-16 h-16 bg-gray-100 object-cover object-center rounded-[5px] mr-4"
-                        src={url}
-                      />
-                    ) : (
-                      <div className="w-16 h-16 bg-gray-100 rounded-full mr-4 flex items-center justify-center">
-                        <PImage size={22} />
+                  <div className="space-y-4 ">
+                    <div className="flex">
+                      {url !== null ? (
+                        <img
+                          alt="team"
+                          className="w-16 h-16 bg-gray-100 object-cover object-center rounded-[5px] mr-4"
+                          src={url}
+                        />
+                      ) : (
+                        <div className="w-16 h-16 bg-gray-100 rounded-full mr-4 flex items-center justify-center">
+                          <PImage size={22} />
+                        </div>
+                      )}
+                      <div>
+                        <h2 className="text-gray-900 title-font font-semibold text-md">
+                          {partner.properties.Name.title[0].plain_text}
+                        </h2>
+                        <p className={`text-gray-500 text-xs`}>
+                          {partner.properties.Specialty.select.name}
+                        </p>
                       </div>
-                    )}
+                    </div>
+                    <div className="flex items-center justify-start md:items-start md:justify-start flex-wrap gap-[10px] md:min-h-[50px]">
+                      {partner.properties.Tags.multi_select
+                        .slice(0, 3)
+                        .map((item) => (
+                          <p
+                            key={item.id}
+                            className="text-gray-900 rounded-sm text-xs bg-gray-200 py-[2px] px-[6px]"
+                          >
+                            {item.name}
+                          </p>
+                        ))}
+                      {partner.properties.Tags.multi_select.length > 3 && (
+                        <p className="rounded-sm text-xs bg-pink-200 py-[2px] px-[6px]">
+                          +{partner.properties.Tags.multi_select.length - 3}{" "}
+                        </p>
+                      )}
+                    </div>
+
                     <div>
-                      <h2 className="text-gray-900 title-font font-semibold text-md">
-                        {partner.properties.Name.title[0].plain_text}
-                      </h2>
-                      <p className={`text-gray-500 text-xs`}>
-                        {partner.properties.Specialty.select.name}
-                      </p>
+                      {partner.properties.Details.rich_text && (
+                        <span className="text-gray-900 rounded-sm text-[13px] line-clamp-4 overflow-hidden text-ellipsis">
+                          {partner.properties.Details.rich_text[0].plain_text}
+                        </span>
+                      )}
                     </div>
                   </div>
-                  <div className="flex items-center justify-start md:items-start md:justify-start flex-wrap gap-[10px]">
-                    {partner.properties.Tags.multi_select.map((item) => (
-                      <p
-                        key={item.id}
-                        className="text-gray-900 rounded-sm text-xs bg-gray-200 py-[2px] px-[6px]"
-                      >
-                        {item.name}
-                      </p>
-                    ))}
-                  </div>
-                  <div>
-                    {partner.properties.Details.rich_text && (
-                      <span className="text-gray-900 rounded-sm text-[13px] line-clamp-4 overflow-hidden text-ellipsis">
-                        {partner.properties.Details.rich_text[0].plain_text}
-                      </span>
-                    )}
-                  </div>
+
                   <Link
                     href={`/partners/${slugToPartnerPage}`}
                     className="flex items-center justify-center text-center bg-[#222C60] font-bold text-white rounded-[2px] py-[10px] text-xs"
@@ -408,24 +379,6 @@ export default function Home() {
               </svg>
             </button>
 
-            {/* Number Buttons */}
-            {/* {Array.from({ length: endPage - startPage + 1 }, (_, index) => {
-              const pageNumber = startPage + index;
-              return (
-                <button
-                  key={pageNumber}
-                  onClick={() => handlePageChange(pageNumber)}
-                  className={`px-4 py-2 mx-1 text-sm font-medium ${
-                    pageNumber === currentPage
-                      ? "bg-blue-500 text-white"
-                      : "bg-gray-200 text-gray-700"
-                  } rounded hover:bg-blue-400`}
-                >
-                  {pageNumber}
-                </button>
-              );
-            })} */}
-
             <button
               onClick={() => handlePageChange(currentPage + 1)}
               disabled={currentPage === totalPages}
@@ -471,6 +424,8 @@ export default function Home() {
           </div>
         </div>
       </section>
-    </main>
+    </>
   );
-}
+};
+
+export default TestingHome;
