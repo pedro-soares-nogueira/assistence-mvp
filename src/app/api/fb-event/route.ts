2ebba0cbdb7f5d96817a6d974c2b4584e9717fe7
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-08-26 11:14:55 -0300

    fixing fb-events route

--- src/app/api/fb-event/route.ts
+++ src/app/api/fb-event/route.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import crypto from "crypto";
 
 const fb_pixel_id = "356841944149440";
@@ -7,85 +7,83 @@ const fb_access_token =
 
 const url = `https://graph.facebook.com/v16.0/${fb_pixel_id}/events?access_token=${fb_access_token}`;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    try {
-      const userData = req.body;
+export async function POST(req: Request) {
+  try {
+    const userData = await req.json();
 
-      if (!userData.event_name) {
-        res.status(400).json({ error: "event_name is required" });
-        return;
-      }
-
-      const event_name = userData.event_name;
-      const client_user_agent = userData.client_user_agent || "";
-      const client_ip_address = userData.client_ip_address || "";
-      const event_time = Math.round(Date.now() / 1000);
-
-      const hashData = (data: string) =>
-        data ? crypto.createHash("sha256").update(data).digest("hex") : "";
-
-      const name = hashData(userData.name);
-      const email = hashData(userData.email);
-      const whatsapp = hashData(userData.whatsapp);
-      const country = hashData(userData.country);
-      const uf = hashData(userData.uf);
-      const city = hashData(userData.city);
-      const cep = hashData(userData.cep);
-      const currency = userData.currency || "";
-
-      const fbc = userData.fbc || "";
-      const fbp = userData.fbp || "";
-      const event_id = userData.event_id || "";
-
-      const user_data = {
-        fn: name,
-        em: email,
-        ph: whatsapp,
-        country: country,
-        st: uf,
-        ct: city,
-        zp: cep,
-        client_user_agent: client_user_agent,
-        client_ip_address: client_ip_address,
-        fbc: fbc,
-        fbp: fbp,
-      };
-
-      const data = {
-        event_name: event_name,
-        event_time: event_time,
-        event_id: event_id,
-        user_data: user_data,
-        action_source: "website",
-        currency: currency,
-        value: 0.0,
-      };
-
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ data: [data] }),
-      });
-
-      if (!response.ok) {
-        res
-          .status(response.status)
-          .json({ error: "Error sending data to Facebook" });
-        return;
-      }
-
-      const responseData = await response.json();
-      res.status(200).json(responseData);
-    } catch (error) {
-      res.status(500).json({ error: "Internal Server Error" });
+    if (!userData.event_name) {
+      return NextResponse.json(
+        { error: "event_name is required" },
+        { status: 400 }
+      );
     }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+
+    const event_name = userData.event_name;
+    const client_user_agent = userData.client_user_agent || "";
+    const client_ip_address = userData.client_ip_address || "";
+    const event_time = Math.round(Date.now() / 1000);
+
+    const hashData = (data: string) =>
+      data ? crypto.createHash("sha256").update(data).digest("hex") : "";
+
+    const name = hashData(userData.name);
+    const email = hashData(userData.email);
+    const whatsapp = hashData(userData.whatsapp);
+    const country = hashData(userData.country);
+    const uf = hashData(userData.uf);
+    const city = hashData(userData.city);
+    const cep = hashData(userData.cep);
+    const currency = userData.currency || "";
+
+    const fbc = userData.fbc || "";
+    const fbp = userData.fbp || "";
+    const event_id = userData.event_id || "";
+
+    const user_data = {
+      fn: name,
+      em: email,
+      ph: whatsapp,
+      country: country,
+      st: uf,
+      ct: city,
+      zp: cep,
+      client_user_agent: client_user_agent,
+      client_ip_address: client_ip_address,
+      fbc: fbc,
+      fbp: fbp,
+    };
+
+    const data = {
+      event_name: event_name,
+      event_time: event_time,
+      event_id: event_id,
+      user_data: user_data,
+      action_source: "website",
+      currency: currency,
+      value: 0.0,
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data: [data] }),
+    });
+
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: "Error sending data to Facebook" },
+        { status: response.status }
+      );
+    }
+
+    const responseData = await response.json();
+    return NextResponse.json(responseData, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
