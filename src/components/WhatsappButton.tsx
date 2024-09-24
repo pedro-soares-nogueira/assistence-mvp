eb66dc6e07b6c11f6f465076bf67f7a3e4f66bc8
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-09-24 16:05:48 -0300

    ajusting email + name

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -9,6 +9,7 @@ interface WhatsProps {
 }
 
 const emailSchema = z.object({
+  name: z.string().min(3, "Nome é obrigatório"),
   email: z.string().email("Digite um e-mail válido."),
 });
 
@@ -26,7 +27,7 @@ const WhatsappButton = ({ linkWhatsApp }: WhatsProps) => {
     resolver: zodResolver(emailSchema),
   });
 
-  const sendEmailToServer = async (email: string) => {
+  const sendEmailToServer = async (name: string, email: string) => {
     try {
       setLoading(true);
       const response = await fetch(
@@ -36,7 +37,7 @@ const WhatsappButton = ({ linkWhatsApp }: WhatsProps) => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ email }),
+          body: JSON.stringify({ name, email }), // Incluímos o nome aqui
         }
       );
 
@@ -59,7 +60,7 @@ const WhatsappButton = ({ linkWhatsApp }: WhatsProps) => {
   };
 
   const onSubmit = (data: EmailFormData) => {
-    sendEmailToServer(data.email);
+    sendEmailToServer(data.name, data.email);
   };
 
   const handleClick = () => {
@@ -88,7 +89,9 @@ const WhatsappButton = ({ linkWhatsApp }: WhatsProps) => {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-md max-w-sm w-full mx-4">
             <div className="flex items-center justify-between gap-4 mb-4">
-              <h2 className="text-xl font-bold">Qual seu melhor e-mail?</h2>
+              <h2 className="text-xl font-bold">
+                Quase lá! Só falta o seu nome e e-mail.
+              </h2>
 
               <button onClick={() => setShowModal(false)}>
                 <svg
@@ -109,8 +112,26 @@ const WhatsappButton = ({ linkWhatsApp }: WhatsProps) => {
             </div>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-4">
+                <label htmlFor="name" className="block text-sm font-medium">
+                  Como quer ser chamado?
+                </label>
+                <input
+                  id="name"
+                  type="name"
+                  {...register("name")}
+                  className="mt-1 p-2 w-full border border-gray-300 rounded"
+                  disabled={loading}
+                />
+                {errors.name && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.name.message}
+                  </p>
+                )}
+              </div>
+
+              <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium">
-                  E-mail
+                  Qual seu melhor email?
                 </label>
                 <input
                   id="email"
@@ -133,7 +154,7 @@ const WhatsappButton = ({ linkWhatsApp }: WhatsProps) => {
                   }`}
                   disabled={loading}
                 >
-                  {loading ? "Enviando..." : "Confirmar"}
+                  {loading ? "Enviando..." : "Chamar profissional"}
                 </button>
               </div>
             </form>
